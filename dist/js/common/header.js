a96932b7d53b120c6ce3665f8860a10a2cdaaa6d
junyanLiu <2509344578@qq.com>
2018-03-30 00:21:51 +0800

    添加头部栏效果

--- dist/js/common/header.js
+++ dist/js/common/header.js
@@ -14,18 +14,45 @@ $(function() {
 		$(this).find('.tool_detail').removeClass('show');
 	});
 
-	(function setTabList() {
-		var tempLocation = window.location.href.split('?');
-		var query = {};
-		if(tempLocation[1]) {
-			var queryArr = tempLocation[1].split('&');
-			queryArr.forEach(function(item, index) {
-				var tempArr = item.split('=');
-				query[tempArr[0]] = tempArr[1];
-			})
-		}
-		
-		var index = query['index'] || 0;
-		$('.tab_item').eq(index).addClass('active');
+	(function() {
+		var offsetArr = [{
+			key: 0,
+			index: 0
+		}];
+		var topTabHeight = $('#topTab').outerHeight();
+		var headerContentHeight = $('#headerContent').outerHeight();
+		$('.model_title').each(function(index, item) {
+			console.log(item, $(item).offset().top);
+			offsetArr.push({
+				key: $(item).offset().top - headerContentHeight - 100,
+				index: $(item).attr('data-index')
+			});
+		});
+		var length = offsetArr.length;
+		console.log(offsetArr);
+		$(document).scroll(function() {
+			var scrollTop = $(this).scrollTop();
+			if (scrollTop > topTabHeight) {
+				$('#header').addClass('fixed');
+			} else {
+				$('#header').removeClass('fixed');
+			}
+			var step = 0;
+			while (step < length) {
+				// console.log(offsetArr[step].key);
+				if (scrollTop >= offsetArr[step].key && scrollTop < offsetArr[step + 1].key) {
+					break;
+				}
+				step = step + 1;
+			}
+			var index = offsetArr[step].index;
+			// console.log(index, '===========', scrollTop);
+			$('#tabList').find('.tab_item').removeClass('active').eq(index).addClass('active');
+		});
+
+		$(document).trigger('scroll');
 	})()
+
+
+
 })
--- src/js/module/index.js
+++ src/js/module/index.js
@@ -1,5 +1,5 @@
 $(function() {
-	var swiperBanner = new Swiper('#banner', {
+    var swiperBanner = new Swiper('#banner', {
         paginationClickable: true,
         nextButton: '#bannerSwiperButtonNext',
         prevButton: '#bannerSwiperButtonPrev',
@@ -16,5 +16,21 @@ $(function() {
         autoplayDisableOnInteraction: false
     });
 
-    
+    var map = new BMap.Map("map"); // 创建地图实例  
+    var point = new BMap.Point(116.404, 39.915); // 创建点坐标 
+
+
+    var myGeo = new BMap.Geocoder();
+    // 将地址解析结果显示在地图上，并调整地图视野    
+    myGeo.getPoint("广东省广州市天河区华夏路富力盈通大厦", function(point) {
+        if (point) {
+            var marker = new BMap.Marker(point); // 创建标注  
+            map.centerAndZoom(point, 16);
+            map.addOverlay(new BMap.Marker(point));
+            map.centerAndZoom(point, 15);
+            map.enableScrollWheelZoom(true);
+            map.addControl(new BMap.NavigationControl());
+            map.addOverlay(marker);
+        }
+    }, "广东省");
 })
