0ec0a1537ca28e3e8e92d3f2da0b50179beebf6f
x <x>
2018-03-29 19:14:35 +0800

    版本一

--- dist/js/module/index.js
+++ dist/js/module/index.js
@@ -1,14 +1,20 @@
 $(function() {
 	var swiperBanner = new Swiper('#banner', {
         paginationClickable: true,
+        nextButton: '#bannerSwiperButtonNext',
+        prevButton: '#bannerSwiperButtonPrev',
         autoplay: 2000,
         loop: true
     });
 
-    var swiperNew = new Swiper('#news', {
+    var dynamicStateBanner = new Swiper('#dynamicState', {
         paginationClickable: true,
-        direction: 'vertical',
+        slidesPerView: 4,
+        slidesPerColumn: 1,
+        spaceBetween: 30,
         autoplay: 2000,
-        loop: true
+        autoplayDisableOnInteraction: false
     });
+
+    
 })
--- src/js/module/help.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	$('#helpList').on('click', '.list_link', function() {
-		$('.list_link').removeClass('active');
-		$(this).addClass('active');
-		$('#helpContentBox').find('.content_item').addClass('hide').eq($(this).attr('data-index')).removeClass('hide');
-	})
-})
